0edb35b5bfc99ce3282d147afe82cc61b02e6d56
atytaev <artem.tutaev.2000@mail.ru>
2024-10-10 19:59:06 +0300

    Add files via upload

---
+++ Ticket reservation service/constants.py
@@ -0,0 +1,7 @@
+DB_CONNECTION = {
+    'host': 'localhost',
+    'port': 5432,
+    'dbname': 'Lesson_16_raw',
+    'user': 'user',
+    'password': 'artemt3858',
+}
---
+++ Ticket reservation service/run.py
@@ -0,0 +1,11 @@
+import math
+import os
+
+from db_backend import DBBackend
+
+def clear_console():
+    os.system('clear')
+
+if __name__ == "__main__":
+    from db import get_event_by_name, get_venue_by_name
+    breakpoint()
