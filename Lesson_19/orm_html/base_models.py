460ccfc7de0fa0a223d29bc14bc63ee30c7e2afb
atytaev <artem.tutaev.2000@mail.ru>
2024-11-06 16:44:46 +0300

    Delete Lesson_19 directory

--- Lesson_19/orm_html/base_models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
-
-
-class BaseModel(Base):
-    __abstract__ = True
-    id = Column(Integer, primary_key=True)
